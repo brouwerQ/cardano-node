3dac0b646e2d78427c83825f6b158d6ce9d42a7d
Denis Shevchenko <denis.shevchenko@iohk.io>
2022-07-29 15:10:51 +0400

    next

--- cardano-tracer/src/Cardano/Tracer/Handlers/RTView/Update/Historical.hs
+++ cardano-tracer/src/Cardano/Tracer/Handlers/RTView/Update/Historical.hs
@@ -10,7 +10,6 @@ module Cardano.Tracer.Handlers.RTView.Update.Historical
   , getAllHistoryFromBackup
   , getLastHistoryFromBackups
   , getLastHistoryFromBackupsAll
-  , restoreHistoryFromBackup
   , runHistoricalBackup
   , runHistoricalUpdater
   ) where
@@ -19,11 +18,11 @@ import           Control.Concurrent.Async (forConcurrently_)
 import           Control.Concurrent.STM (atomically)
 import           Control.Concurrent.STM.TVar (modifyTVar', readTVar, readTVarIO)
 import           Control.Exception.Extra (ignore, try_)
-import           Control.Monad (forM, forM_, forever, unless)
+import           Control.Monad (forM, forM_, forever)
 import           Control.Monad.Extra (ifM, whenJust)
 import qualified Data.ByteString.Lazy as BSL
 import qualified Data.Csv as CSV
-import           Data.List (find, isInfixOf, partition)
+import           Data.List (find, isInfixOf)
 import           Data.Map.Strict (Map)
 import qualified Data.Map.Strict as M
 import           Data.Maybe (catMaybes)
@@ -73,9 +72,8 @@ runHistoricalUpdater tracerEnv lastResources = forever $ do
   sleep 1.0 -- TODO: should it be configured?
   now <- systemToUTCTime <$> getSystemTime
   allMetrics <- readTVarIO teAcceptedMetrics
-  forM_ (M.toList allMetrics) $ \(nodeId, (ekgStore, _)) -> do
-    metrics <- getListOfMetrics ekgStore
-    forM_ metrics $ \(metricName, metricValue) -> do
+  forM_ (M.toList allMetrics) $ \(nodeId, (ekgStore, _)) ->
+    forMM_ (getListOfMetrics ekgStore) $ \(metricName, metricValue) -> do
       updateTransactionsHistory nodeId teTxHistory metricName metricValue now
       updateResourcesHistory nodeId teResourcesHistory lastResources metricName metricValue now
       updateBlockchainHistory nodeId teBlockchainHistory metricName metricValue now
@@ -88,9 +86,9 @@ runHistoricalUpdater tracerEnv lastResources = forever $ do
 runHistoricalBackup :: TracerEnv -> IO ()
 runHistoricalBackup tracerEnv@TracerEnv{teRTViewPageOpened} = forever $ do
   sleep 300.0 -- TODO: 5 minutes, should it be changed?
-  readTVarIO teRTViewPageOpened >>= \case
-    False -> backupAllHistory tracerEnv
-    True -> return () -- Skip, UI-code is performing backup.
+  ifM (readTVarIO teRTViewPageOpened)
+    (return ()) -- Skip, UI-code is performing backup.
+    (backupAllHistory tracerEnv)
 
 backupAllHistory :: TracerEnv -> IO ()
 backupAllHistory tracerEnv@TracerEnv{teConnectedNodes} = do
@@ -130,10 +128,9 @@ backupSpecificHistory
   -> DataName
   -> IO ()
 backupSpecificHistory tracerEnv history connected dataName = do
-  nodesIdsWithNames <- getNodesIdsWithNames tracerEnv connected
   backupDir <- getPathToBackupDir
   hist <- readTVarIO history
-  forM_ nodesIdsWithNames $ \(nodeId, nodeName) -> do
+  forMM_ (getNodesIdsWithNames tracerEnv connected) $ \(nodeId, nodeName) -> do
     backupHistory backupDir hist nodeId nodeName $ Just dataName
     cleanupSpecificHistoryPoints nodeId
  where
@@ -176,14 +173,6 @@ backupHistory backupDir history nodeId nodeName mDataName =
       (BSL.writeFile  historyDataFile $ pointsToBS historyPoints)
 
   pointsToBS = CSV.encode . S.toAscList
-
-data HistoryMark = LatestHistory | AllHistory
-
-restoreHistoryFromBackup
-  :: TracerEnv
-  -> Set NodeId
-  -> IO ()
-restoreHistoryFromBackup = restoreHistoryFromBackup' LatestHistory Nothing
 
 getAllHistoryFromBackup
   :: TracerEnv
@@ -262,148 +251,6 @@ getLastHistoryFromBackups' tracerEnv nodeIds = do
                     !firstTSWeNeed = utc2s now - sixHoursInS
                     pointsWeNeed = filter (\(ts, _) -> ts >= firstTSWeNeed) $ V.toList pointsV
                 return $ Just (dataName, pointsWeNeed)
-
-
-
-
-
-
-
-
-
-
-
-
-
-restoreHistoryFromBackup'
-  :: HistoryMark
-  -> Maybe DataName
-  -> TracerEnv
-  -> Set NodeId
-  -> IO ()
-restoreHistoryFromBackup' historyMark aDataName tracerEnv connected = ignore $ do
-  nodesIdsWithNames <- getNodesIdsWithNames tracerEnv (S.toList connected)
-  backupDir <- getPathToBackupDir
-  forM_ nodesIdsWithNames $ \(nodeId, nodeName) -> do
-    let nodeSubdir = backupDir </> T.unpack nodeName
-    doesDirectoryExist nodeSubdir >>= \case
-      False -> return () -- There is no backup for this node.
-      True -> do
-        backupFiles <- listFiles nodeSubdir
-        -- Check if we need a history for all historical data or for particular one only.
-        namesWithPoints <-
-          case aDataName of
-            Nothing ->
-              forM backupFiles $
-                extractNamesWithHistoricalPoints nodeSubdir
-            Just oneDataName ->
-              case find (\bFile -> show oneDataName `isInfixOf` bFile) backupFiles of
-                Nothing -> return []
-                Just backupFile -> do
-                  nameWithPoints <- extractNamesWithHistoricalPoints nodeSubdir backupFile
-                  return [nameWithPoints]
-
-        fillHistory nodeId chainHistory     chainData namesWithPoints
-        fillHistory nodeId resourcesHistory resData   namesWithPoints
-        fillHistory nodeId txHistory        txData    namesWithPoints
- where
-  TracerEnv{teBlockchainHistory, teResourcesHistory, teTxHistory} = tracerEnv
-  ChainHistory chainHistory   = teBlockchainHistory
-  ResHistory resourcesHistory = teResourcesHistory
-  TXHistory txHistory         = teTxHistory
-
-  extractNamesWithHistoricalPoints nodeSubdir bFile = do
-    let pureFile = takeBaseName bFile
-    case readMaybe pureFile of
-      Nothing -> return noPoints
-      Just (dataName :: DataName) -> do
-        -- Ok, this file contains historical points for 'dataName', extract them...
-        let backupFile = nodeSubdir </> pureFile
-        try_ (BSL.readFile backupFile) >>= \case
-          Left _ -> return noPoints
-          Right rawPoints ->
-            case CSV.decode CSV.NoHeader rawPoints of
-              Left _ -> return noPoints -- Maybe file was broken...
-              Right (pointsV :: V.Vector HistoricalPoint) -> do
-                let points = V.toList pointsV
-                if null points
-                  then return noPoints
-                  else
-                    -- Now we extracted all the points from this backup file.
-                    -- Check if we need all of them or the latest ones only.
-                    case historyMark of
-                      LatestHistory -> do
-                        now <- systemToUTCTime <$> getSystemTime
-                        -- Ok, take the points for the last 6 hours, and only these
-                        -- points will be rendered on JS-charts.
-                        let sixHoursInS = 21600
-                            !firstTSWeNeed = utc2s now - sixHoursInS
-                            (earlyPoints, pointsWeNeed) =
-                              partition (\(ts, _) -> ts < firstTSWeNeed) points
-                        unless (null pointsWeNeed) $
-                          -- Now we re-write backup file with all the points
-                          -- except points we need now. These "last" points
-                          -- will be stored in this file again (with the new points)
-                          -- during the first backup.
-                          BSL.writeFile backupFile $ CSV.encode earlyPoints
-                        return (Just dataName, S.fromList pointsWeNeed)
-                      AllHistory ->
-                        -- Ok, take all the history.
-                        return (Just dataName, S.fromList points)
-
-  noPoints = (Nothing, S.empty)
-
-  fillHistory _ _ _ [] = return ()
-  fillHistory nodeId history dataNames dataNamesWithPoints = do
-    let backupData = mkMapOfData dataNamesWithPoints M.empty
-    atomically . modifyTVar' history $ \currentHistory ->
-      case M.lookup nodeId currentHistory of
-        Nothing -> M.insert nodeId backupData currentHistory
-        Just currentData -> M.adjust (const $ M.unionWith S.union currentData backupData)
-                                     nodeId currentHistory
-   where
-    mkMapOfData [] aMap = aMap
-    mkMapOfData ((mDataName, points):others) aMap =
-      case mDataName of
-        Nothing -> mkMapOfData others aMap
-        Just dataName ->
-          if dataName `elem` dataNames
-            then mkMapOfData others $ M.insert dataName points aMap
-            else mkMapOfData others aMap
-
-  chainData =
-    [ ChainDensityData
-    , SlotNumData
-    , BlockNumData
-    , SlotInEpochData
-    , EpochData
-    , NodeCannotForgeData
-    , ForgedSlotLastData
-    , NodeIsLeaderData
-    , NodeIsNotLeaderData
-    , ForgedInvalidSlotLastData
-    , AdoptedSlotLastData
-    , NotAdoptedSlotLastData
-    , AboutToLeadSlotLastData
-    , CouldNotForgeSlotLastData
-    ]
-
-  resData =
-    [ CPUData
-    , MemoryData
-    , GCMajorNumData
-    , GCMinorNumData
-    , GCLiveMemoryData
-    , CPUTimeGCData
-    , CPUTimeAppData
-    , ThreadsNumData
-    ]
-
-  txData =
-    [ TxsProcessedNumData
-    , MempoolBytesData
-    , TxsInMempoolData
-    ]
 
 getNodesIdsWithNames
   :: TracerEnv
